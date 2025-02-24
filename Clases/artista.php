896bbb5358f94b10d364c1002f16b27d339338c4
josefernandezzz <josefdzmrn@gmail.com>
2025-02-24 16:46:38 +0100

    Cambio la clase artista y creo la libreria buscar artistas en la API

--- Clases/artista.php
+++ Clases/artista.php
@@ -1,25 +1,12 @@
 <?php
-require_once('Librerias/render.php');
+final class artista
+{
 
-$objHTML = new RenderHTML();
+    public function __construct($nombre) {
+        $this->nombre=$nombre;
+    }
+    public $nombre;
 
-$objHTML->RenderHeader();
-?>
+}
 
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-
-
-<?php
-    $objHTML->RenderFooter();
 ?>
