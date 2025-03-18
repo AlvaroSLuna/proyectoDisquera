21b9f1966393d2126278636375a3e98bacfcf6a7
rafaameedina <rafamedinaproyectos@gmail.com>
2025-03-18 13:08:12 +0100

    Actualizaciones

--- Librerias/renderIndex.php
+++ Librerias/renderIndex.php
@@ -39,7 +39,7 @@ class RenderHTML
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="/index.php">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/">Novedades</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/index.php#novedades">Novedades</a></li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="artistasDropdown" role="button"
                             data-bs-toggle="dropdown">Artistas</a>
